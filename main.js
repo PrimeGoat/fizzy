a7124bf3ed6b4dc6b19b849664eb71d69bea1040
DENIS SAVGIR <denis@DENISs-MacBook-Pro.local>
2020-02-04 15:02:23 -0500

    Replaced == with ===

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function fizzy(num) {
   let output = "";
 
-  if(num % 3 == 0) {
+  if(num % 3 === 0) {
     output += "Fizz";
   }
-  if(num % 5 == 0) {
+  if(num % 5 === 0) {
     output += "Buzz";
   }
 
