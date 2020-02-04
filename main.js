1c99f0d684ea92fed5cfb1d3500924787d5cb2b7
DENIS SAVGIR <denis@DENISs-MacBook-Pro.local>
2020-02-04 14:17:25 -0500

    Done.

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 function fizzy(num) {
+  let output = "";
+  if(num % 3 == 0) {
+    output += "Fizz";
+  }
+  if(num % 5 == 0) {
+    output += "Buzz";
+  }
 
+  if(output.length > 0) {
+    return output;
+  } else {
+    return num;
+  }
 }
 
 module.exports = {
